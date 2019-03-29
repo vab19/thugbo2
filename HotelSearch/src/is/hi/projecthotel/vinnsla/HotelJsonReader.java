7e80a4dd2bfd5aa9a0c5e7c0c24e60bdc675e2f5
nth15 <nth15@hi.is>
2019-03-29 12:08:52 +0000

    Jsonreader init

---
+++ HotelSearch/src/is/hi/projecthotel/vinnsla/HotelJsonReader.java
@@ -0,0 +1,16 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.json.JsonValue;
+
+
+public class HotelJsonReader{
+    public static final String JSON_FILE="HotelList.json";
+    
+    
+}
