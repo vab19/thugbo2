13b79af873d39cd80fbe6cb1df13200347dfa40a
nth15 <nth15@hi.is>
2019-03-29 21:27:25 +0000

    Tók út comment í test klasanum

--- HotelSearch/test/is/hi/projecthotel/vinnsla/HotelTest.java
+++ HotelSearch/test/is/hi/projecthotel/vinnsla/HotelTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package is.hi.projecthotel.vinnsla;
 
 import java.util.ArrayList;
@@ -13,10 +8,6 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author nokkv
- */
 public class HotelTest {
     
     public HotelTest() {
