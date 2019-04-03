7605c8929788b7004479f9e40d776efbd4755ad9
nth15 <nth15@hi.is>
2019-04-03 17:29:16 +0000

    Jackson

--- thugbo2-Valdimar/HotelSearch/src/is/hi/projecthotel/vinnsla/HotelJsonGenerator.java
+++ thugbo2-Valdimar/HotelSearch/src/is/hi/projecthotel/vinnsla/HotelJsonGenerator.java
@@ -1,13 +1,45 @@
 package is.hi.projecthotel.vinnsla;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.List;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
 public class HotelJsonGenerator {
 
-    public void Generate() {
-        // this.arrayList 
-        /**
-         * TODO 
-         * Parse í json form 
-         * og yfirskrifa json
-         */
+    public void generate() {
+    }
+    
+    public static void main(String[] args) {
+        int herb = 10;
+        Hotel h = new Hotel(herb);
+        h.setHotelId(12);
+        h.setHotelName("Hótel Hafnafjörðut");
+        h.setAddress("Eyravegur 22");
+        h.setRegion("Suðurland");
+        h.setPricePernight(10000);
+        h.setStars(4);
+        String rev = "Geggjað gott";
+        int rat = 10;
+        int cust = 55;
+        h.addReview(rev, rat, cust, 12);
+     
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.writeValue(new File("Hotel.json"), h);
     }
 }
