187cd38ac5282ea054d4981d0300a2e803653440
nth15 <nth15@hi.is>
2019-04-03 18:25:15 +0000

    Proof of concept json reader

--- thugbo2-Valdimar/HotelSearch/src/is/hi/projecthotel/vinnsla/HotelJsonReader.java
+++ thugbo2-Valdimar/HotelSearch/src/is/hi/projecthotel/vinnsla/HotelJsonReader.java
@@ -1,5 +1,16 @@
 package is.hi.projecthotel.vinnsla;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.ArrayList;
+
 import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -12,5 +23,13 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 
 public class HotelJsonReader {
 
+    public static void main(String[] args) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        ArrayList<Hotel> hotelList = new ArrayList<>();
+        
+        hotelList = objectMapper.readValue(new File("HotelList.json"), ArrayList.class);
+        System.out.println(hotelList.get(0));
+    }
     
 }
