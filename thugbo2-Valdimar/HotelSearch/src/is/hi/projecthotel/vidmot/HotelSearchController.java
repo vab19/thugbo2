fff3d9804be267fdcc28694189ea6ac4cb9bccfd
Breki <bri11@hi.is>
2019-04-08 20:20:25 +0000

    Semi raðar hlutum í stafrósröð en ekki alveg rétt

--- thugbo2-Valdimar/HotelSearch/src/is/hi/projecthotel/vidmot/HotelSearchController.java
+++ thugbo2-Valdimar/HotelSearch/src/is/hi/projecthotel/vidmot/HotelSearchController.java
@@ -24,6 +24,7 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.MultipleSelectionModel;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
@@ -32,14 +33,15 @@ import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import java.util.Arrays;
 
 public class HotelSearchController implements Initializable {
-    @FXML
-    private HotelInfoController hotelInfo;
     private Hotel[] hotels;
+    private String[] listItems;
     
     List<Hotel> hotelList;
-    private VBox vboxdude;
+    
+    //private VBox vboxdude;
     @FXML
     private Button HOTELPAGEBUTTON;
     @FXML
@@ -59,25 +61,30 @@ public class HotelSearchController implements Initializable {
     private int indexer = 0;
     @FXML
     private VBox vBoxdude;
+    @FXML
+    private TextField SearchParam;
     
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        hotels = new Hotel [40];
-       
-        
+        int length = 40;
+        hotels = new Hotel [length];
+        listItems = new String[length];
+        String item = "";
         //ObservableList<String> items = FXCollections.observableArrayList("test1", "test2");
         list = new ListView<String>();
-        for(int i=0;i<40;i++) {
+        for(int i=0;i<length;i++) {
             
             //HBox element = new HBox();
            /* vboxdude.getChildren().add(new Text(hotel.getHotelName()+" |"+hotel.getStars()
             +" |"+hotel.getRegion()+" |"+hotel.getPricePerNight()+" |"+hotel.getRating()));
             */
-           
             hotels[i] = CreateMockHotels.randomHotel(i);
-            list.getItems().addAll(""+hotels[i].getHotelName()
-            +"    |    "+hotels[i].getRegion()+"    |    "+hotels[i].getPricePerNight()+"    |    "+hotels[i].getRating());
+            item = ""+hotels[i].getHotelName()
+            +"    |    "+hotels[i].getRegion()+"    |    "+hotels[i].getPricePerNight()+"    |    "+hotels[i].getRating();
+            listItems[i] = item;
+            
+            list.getItems().addAll(item);
            
         }
         
@@ -132,13 +139,69 @@ public class HotelSearchController implements Initializable {
 
     @FXML
     private void FILTEREVENT(ActionEvent event) {
-        int length = 40;
-        System.out.println("" + length);
-        String[] elements = new String[length];
-        for(int i=0;i<length;i++) {
+        //NAME, REGI, PRIC, RATI, 10-14
+        
+        int length = hotels.length;
+        String buttonType = event.getSource().toString().substring(10, 14);
+        String[] listItemsPre = new String[length];
+        String[] listItemsPost = new String[length];
+        int j;
+        if("NAME".equals(buttonType)) {
+            for(int i=0;i<length;i++) {
+                listItemsPost[i] = listItems[i];
+            }
+            Arrays.sort(listItemsPost);
+            list = new ListView<String>();
+            for(int i=0;i<40;i++) {
+                    list.getItems().addAll(listItemsPost[i]);
+            }
             
+            
+        } else if("REGI".equals(buttonType)) {
+            for(int i=0;i<length;i++) {
+                j = listItems[i].indexOf("|");
+                listItemsPre[i] = listItems[i].substring(0, j-1);
+                listItemsPost[i] = listItems[i].substring(j);
+            }
+            Arrays.sort(listItemsPost);
+            list = new ListView<String>();
+            for(int i=0;i<40;i++) {
+                    list.getItems().addAll(listItemsPre[i]+listItemsPost[i]);
+            }
+        } else if("PRIC".equals(buttonType)) {
+            for(int i=0;i<length;i++) {
+                j = listItems[i].indexOf("|");
+                j = listItems[i].indexOf("|", j+1);
+                listItemsPre[i] = listItems[i].substring(0, j-1);
+                listItemsPost[i] = listItems[i].substring(j);
+            }
+            Arrays.sort(listItemsPost);
+            list = new ListView<String>();
+            for(int i=0;i<40;i++) {
+                    list.getItems().addAll(listItemsPre[i]+listItemsPost[i]);
+            }
+        
+        } else if("RATI".equals(buttonType)) {
+            for(int i=0;i<length;i++) {
+                //finnur þriðja instancið af |
+                j = listItems[i].indexOf("|");
+                j = listItems[i].indexOf("|", j+1);
+                j = listItems[i].indexOf("|", j+1);
+                listItemsPre[i] = listItems[i].substring(0, j-1);
+                listItemsPost[i] = listItems[i].substring(j);
+            }
+            Arrays.sort(listItemsPost);
+            list = new ListView<String>();
+            for(int i=0;i<40;i++) {
+                    list.getItems().addAll(listItemsPre[i]+listItemsPost[i]);
+            }
+        } else {
+            System.out.println("no button found");
         }
-        System.out.println("" + index);
+        
+        
+        vBoxdude.getChildren().remove(0);
+        vBoxdude.getChildren().addAll(list);
     }
      private void frumstillaGognHandlerListi() {
         msl = list.getSelectionModel();
@@ -154,4 +217,23 @@ public class HotelSearchController implements Initializable {
         
         
 }
+
+    @FXML
+    private void SearchForEvent(ActionEvent event) {
+        String param = SearchParam.getText();
+        System.out.println(param);
+        String item = "";
+        list = new ListView<String>();
+        for(int i=0;i<40;i++) {
+            System.out.println(listItems[i].contains(param));
+            if(listItems[i].contains(param)) {
+                list.getItems().addAll(listItems[i]);
+                System.out.println(listItems[i]);
+            }
+        }
+        //vBoxdude = new VBox();
+        
+        vBoxdude.getChildren().remove(0);
+        vBoxdude.getChildren().addAll(list);
+    }
 }
