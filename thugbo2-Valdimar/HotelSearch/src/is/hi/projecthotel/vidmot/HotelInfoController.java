ae18e40ddbe38cc03eb369c480abef6b07ba2566
Breki <bri11@hi.is>
2019-04-08 17:01:54 +0000

    started woking on filter

--- thugbo2-Valdimar/HotelSearch/src/is/hi/projecthotel/vidmot/HotelInfoController.java
+++ thugbo2-Valdimar/HotelSearch/src/is/hi/projecthotel/vidmot/HotelInfoController.java
@@ -5,9 +5,13 @@
  */
 package is.hi.projecthotel.vidmot;
 
+import is.hi.projecthotel.vinnsla.Hotel;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
 /**
  * FXML Controller class
@@ -16,12 +20,41 @@ import javafx.fxml.Initializable;
  */
 public class HotelInfoController implements Initializable {
 
+    @FXML
+    private Label NameLabel;
+    @FXML
+    private Label RatingLabel;
+    @FXML
+    private Label StarsLabel;
+    @FXML
+    private Label RegionLabel;
+    @FXML
+    private Label AddressLabel;
+    @FXML
+    private Label RoomsAvailableLabel;
+    @FXML
+    private Label PriceLabel;
+    
+    //private OpenScene openScene;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
+
+    public void displayHotelInfo(Hotel h) {
+        
+    }
+
+    @FXML
+    private void BookRoomEvent(ActionEvent event) {
+    }
+
+    @FXML
+    private void CancelEvent(ActionEvent event) {
+    }
     
 }
