a23843ab45a52b96524f13842d286aa525561a77
Breki <bri11@hi.is>
2019-04-08 17:51:09 +0000

    started woking on filter

--- thugbo2-Valdimar/HotelSearch/src/is/hi/projecthotel/vidmot/HotelSearchController.java
+++ thugbo2-Valdimar/HotelSearch/src/is/hi/projecthotel/vidmot/HotelSearchController.java
@@ -1,6 +1,7 @@
 package is.hi.projecthotel.vidmot;
 
 import is.hi.projecthotel.vinnsla.*;
+import java.io.IOException;
 import java.net.URL;
 
 import java.util.ArrayList;
@@ -13,8 +14,11 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.MultipleSelectionModel;
@@ -25,9 +29,13 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class HotelSearchController implements Initializable {
-    
+    @FXML
+    private HotelInfoController hotelInfo;
     private Hotel[] hotels;
     
     List<Hotel> hotelList;
@@ -89,12 +97,23 @@ public class HotelSearchController implements Initializable {
     }
 
     @FXML
-    private void OPENHOTELINFO(MouseEvent event) {
+    private void OPENHOTELINFO(MouseEvent event) throws IOException {
         int indexer = list.getSelectionModel().getSelectedIndex();
         if(indexer != -1) {
-            //hotelInfo.displayHotelInfo(hotels[indexer]);
             
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("HotelInfo.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            //stage.initModality(Modality.APPLICATION_MODAL);
+            //stage.initStyle(StageStyle.UNDECORATED);
+            //stage.setTitle("ABC");
+            
+            stage.setScene(new Scene(root1));
+            //stage.
+            stage.show();
+            //hotelInfo.displayHotelInfo(hotels[indexer]);
         }
+            
         
         System.out.println(""+indexer);
        
