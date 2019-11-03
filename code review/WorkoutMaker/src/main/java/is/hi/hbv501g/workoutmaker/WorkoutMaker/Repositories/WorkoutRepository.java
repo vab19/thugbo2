7b9962d59807622becc3a0731337c913c83be047
OlafurEinar <olieinar@hotmail.com>
2019-11-03 17:47:22 +0000

    code review

---
+++ code review/WorkoutMaker/src/main/java/is/hi/hbv501g/workoutmaker/WorkoutMaker/Repositories/WorkoutRepository.java
@@ -0,0 +1,23 @@
+package is.hi.hbv501g.workoutmaker.WorkoutMaker.Repositories;
+
+
+import is.hi.hbv501g.workoutmaker.WorkoutMaker.Entities.Workout;
+import is.hi.hbv501g.workoutmaker.WorkoutMaker.Entities.WorkoutType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface WorkoutRepository extends JpaRepository<Workout, Long> {
+    Workout save(Workout workout);
+    void delete(Workout workout);
+    void deleteAll();
+    List<Workout> findAll();
+    List<Workout> findByWorkoutName(String workoutName);
+    Optional<Workout> findById(long id);
+    List<Workout> findByWorkoutType(WorkoutType workoutType);
+    boolean existsById (long id);
+    boolean existsByWorkoutName (String workoutName);
+
+}
